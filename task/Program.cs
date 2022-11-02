c533ae9d82e53fcb5160cbb34c5eca88be291284
ElenaLep <112198873+elepshchikova@users.noreply.github.com>
2022-11-03 02:14:45 +0300

    Update Program.cs

--- task/Program.cs
+++ task/Program.cs
@@ -1,27 +1,45 @@
-﻿Console.Write("Введите количество элементов массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-string[] arrayStrings = new string[size];
-int len = 3;
-int pos = 0;
-for (int i = 0; i < size; i++)
+using System;
+using static System.Console;
+
+Clear();
+
+string[] array = AskArray();
+string[] result = FindLessThan(array, 3);
+WriteLine($"[{string.Join(", ", array)}] -> [{string.Join(", ", result)}]");
+
+string[] FindLessThan(string[] input, int n)
 {
- Console.WriteLine($"Введите {i+1}-й элемент: ");
- string element = Convert.ToString(Console.ReadLine());
- if (element.Length <= len)
+    string[] output = new string[CountLessThan(input, n)];
+
+    for (int i = 0, j = 0; i < input.Length; i++)
     {
- arrayStrings[pos] = element;
- pos++;
+        if (input[i].Length <= n)
+        {
+            output[j] = input[i];
+            j++;
+        }
     }
-}
-Console.WriteLine();
-PrintArray(arrayStrings);
-void PrintArray(string[] array)
-{
-    for (int i = 0; i < array.Length; i++)
-    {
-        Console.Write(array[i] + " ");
-    }
-    Console.WriteLine();
+
+    return output;
 }
 
+int CountLessThan(string[] input, int n)
+{
+    int count = 0;
 
+    for (int i = 0; i < input.Length; i++)
+    {
+        if (input[i].Length <= n)
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+string[] AskArray()
+{
+    Write("Введите значения через пробел: ");
+    return ReadLine().Split(" ");
+}
